5ad60716e695e520f938cfc460f00f6e4bf83a9f
miloit <MichaelLoercher@web.de>
2020-12-13 16:09:34 +0100

    Initial

---
+++ kodi.pro
@@ -0,0 +1,97 @@
+TEMPLATE  = lib
+CONFIG   += plugin
+QT       += core quick network
+
+# === Version and build information ===========================================
+# If built in Buildroot use custom package version, otherwise Git
+isEmpty(YIO_BUILD_VERSION) {
+    GIT_VERSION = "$$system(git describe --match "v[0-9]*" --tags HEAD --always)"
+    GIT_HASH = "$$system(git log -1 --format="%H")"
+    GIT_BRANCH = "$$system(git rev-parse --abbrev-ref HEAD)"
+} else {
+    GIT_VERSION = $$YIO_BUILD_VERSION
+    contains(GIT_VERSION, "^v?(0|[1-9]\d*)\..*") {
+        # (simplified) version string = regular release
+        GIT_HASH = ""
+        GIT_BRANCH = "master"
+    } else {
+        # git hash as version = custom build
+        GIT_HASH = $$YIO_BUILD_VERSION
+        GIT_BRANCH = ""
+    }
+}
+KODI_VERSION = $$replace(GIT_VERSION, v, "")
+DEFINES += PLUGIN_VERSION=\\\"$$KODI_VERSION\\\"
+
+# build timestamp
+win32 {
+    # not the same format as on Unix systems, but good enough...
+    BUILDDATE=$$system(date /t)
+} else {
+    BUILDDATE=$$system(date +"%Y-%m-%dT%H:%M:%S")
+}
+# =============================================================================
+
+CONFIG(debug, debug|release) {
+    DEBUG_BUILD = true
+} else {
+    DEBUG_BUILD = false
+}
+
+INTG_LIB_PATH = $$(YIO_SRC)
+isEmpty(INTG_LIB_PATH) {
+    INTG_LIB_PATH = $$clean_path($$PWD/../integrations.library)
+    message("Environment variables YIO_SRC not defined! Using '$$INTG_LIB_PATH' for integrations.library project.")
+} else {
+    INTG_LIB_PATH = $$(YIO_SRC)/integrations.library
+    message("YIO_SRC is set: using '$$INTG_LIB_PATH' for integrations.library project.")
+}
+
+! include($$INTG_LIB_PATH/qmake-destination-path.pri) {
+    error( "Couldn't find the qmake-destination-path.pri file!" )
+}
+
+! include($$INTG_LIB_PATH/yio-plugin-lib.pri) {
+    error( "Cannot find the yio-plugin-lib.pri file!" )
+}
+
+! include($$INTG_LIB_PATH/yio-model-mediaplayer.pri) {
+    error( "Cannot find the yio-model-mediaplayer.pri file!" )
+}
+
+# verify integrations.library version
+unix {
+    INTG_LIB_VERSION = $$system(cat $$PWD/dependencies.cfg | awk '/^integrations.library:/$$system_quote("{print $2}")')
+    INTG_GIT_VERSION = "$$system(cd $$INTG_LIB_PATH && git describe --match "v[0-9]*" --tags HEAD --always)"
+    INTG_GIT_BRANCH  = "$$system(cd $$INTG_LIB_PATH && git rev-parse --abbrev-ref HEAD)"
+    message("Required integrations.library version: $$INTG_LIB_VERSION Local version: $$INTG_GIT_VERSION ($$INTG_GIT_BRANCH)")
+    # this is a simple check but qmake only provides limited tests and 'versionAtLeast' doesn't work with 'v' prefix.
+    !contains(INTG_GIT_VERSION, $$re_escape($${INTG_LIB_VERSION}).*)) {
+        !equals(INTG_GIT_BRANCH, $$INTG_LIB_VERSION) {
+            error("Invalid integrations.library version: \"$$INTG_GIT_VERSION\". Please check out required version \"$$INTG_LIB_VERSION\"")
+        }
+    }
+}
+
+QMAKE_SUBSTITUTES += kodi.json.in version.txt.in
+# output path must be included for the output file from QMAKE_SUBSTITUTES
+INCLUDEPATH += $$OUT_PWD
+HEADERS  += src/kodi.h
+SOURCES  += src/kodi.cpp
+TARGET    = kodi
+
+# Configure destination path. DESTDIR is set in qmake-destination-path.pri
+DESTDIR = $$DESTDIR/plugins
+OBJECTS_DIR = $$PWD/build/$$DESTINATION_PATH/obj
+MOC_DIR = $$PWD/build/$$DESTINATION_PATH/moc
+RCC_DIR = $$PWD/build/$$DESTINATION_PATH/qrc
+UI_DIR = $$PWD/build/$$DESTINATION_PATH/ui
+
+DISTFILES += \
+    dependencies.cfg \
+    kodi.json.in \
+    version.txt.in \
+    README.md
+
+# Add setup schema to metadata
+CFG_SCHEMA = "$$cat($$PWD/setup-schema.json)"
---
+++ src/kodi.h
@@ -0,0 +1,129 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2019 Marton Borzak <hello@martonborzak.com>
+ *
+ * This file is part of the YIO-Remote software project.
+ *
+ * YIO-Remote software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YIO-Remote software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YIO-Remote software. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *****************************************************************************/
+
+#pragma once
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QTimer>
+
+#include "yio-interface/entities/mediaplayerinterface.h"
+#include "yio-model/mediaplayer/albummodel_mediaplayer.h"
+#include "yio-model/mediaplayer/searchmodel_mediaplayer.h"
+#include "yio-plugin/integration.h"
+#include "yio-plugin/plugin.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// SPOTIFY FACTORY
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const bool USE_WORKER_THREAD = false;
+
+class SpotifyPlugin : public Plugin {
+    Q_OBJECT
+    Q_INTERFACES(PluginInterface)
+    Q_PLUGIN_METADATA(IID "YIO.PluginInterface" FILE "spotify.json")
+
+ public:
+    SpotifyPlugin();
+
+    // Plugin interface
+ protected:
+    Integration* createIntegration(const QVariantMap& config, EntitiesInterface* entities,
+                                   NotificationsInterface* notifications, YioAPIInterface* api,
+                                   ConfigInterface* configObj) override;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// SPOTIFY CLASS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Spotify : public Integration {
+    Q_OBJECT
+
+ public:
+    explicit Spotify(const QVariantMap& config, EntitiesInterface* entities, NotificationsInterface* notifications,
+                     YioAPIInterface* api, ConfigInterface* configObj, Plugin* plugin);
+
+    void sendCommand(const QString& type, const QString& entitId, int command, const QVariant& param) override;
+
+ public slots:  // NOLINT open issue: https://github.com/cpplint/cpplint/pull/99
+    void connect() override;
+    void disconnect() override;
+    void enterStandby() override;
+    void leaveStandby() override;
+
+ signals:
+    void requestReady(const QVariantMap& obj, const QString& url);
+
+ private:
+    // Spotify API calls
+    void search(QString query);
+    void search(QString query, QString type);
+    void search(QString query, QString type, QString limit, QString offset);
+    void getAlbum(QString id);
+    void getPlaylist(QString id);
+    void getUserPlaylists();
+
+    // Spotify API authentication
+    void refreshAccessToken();
+
+    // Spotify Connect API calls
+    void getCurrentPlayer();
+
+    void updateEntity(const QString& entity_id, const QVariantMap& attr);
+
+    // get and post requests
+    void getRequest(const QString& url, const QString& params);
+    void postRequest(const QString& url, const QString& params);
+    void putRequest(const QString& url, const QString& params);  // TODO(marton): change param to QUrlQuery
+                                                                 // QUrlQuery query;
+
+    //    query.addQueryItem("username", "test");
+    //    query.addQueryItem("password", "test");
+
+    //    url.setQuery(query.query());
+
+ private slots:  // NOLINT open issue: https://github.com/cpplint/cpplint/pull/99
+    void onTokenTimeOut();
+    void onPollingTimerTimeout();
+    void onProgressBarTimerTimeout();
+
+ private:
+    bool    m_startup = true;
+    QString m_entityId;
+
+    // polling timer
+    QTimer* m_pollingTimer;
+    QTimer* m_progressBarTimer;
+
+    int m_progressBarPosition = 0;
+
+    // Spotify auth stuff
+    QString m_clientId;
+    QString m_clientSecret;
+    QString m_accessToken;
+    QString m_refreshToken;
+    int     m_tokenExpire = 0;  // in seconds
+    QTimer* m_tokenTimeOutTimer;
+    QString m_apiURL = "https://api.spotify.com";
+};
